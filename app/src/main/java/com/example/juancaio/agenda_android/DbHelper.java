b99d21d0a9797c2543d4608e0c163211e4e90d15
jcpsantos <juancaiops@gmail.com>
2018-05-02 23:31:26 -0300

    melhorias na classe dbhelper e no layout de cadastro

--- app/src/main/java/com/example/juancaio/agenda_android/DbHelper.java
+++ app/src/main/java/com/example/juancaio/agenda_android/DbHelper.java
@@ -11,7 +11,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DbHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "Crud.db";
     private static final int DATABASE_VERSION = 1;
-    private final String CREATE_TABLE = "CREATE TABLE Contato (ID INTEGER PRIMARY KEY AUTOINCREMENT, Nome TEXT NOT NULL, Telefone TEXT NOT NULL, Celular TEXT NOT NULL, Email TEXT NOT NULL);";
+    private final String CREATE_TABLE = "CREATE TABLE Contatos (ID INTEGER PRIMARY KEY AUTOINCREMENT, Nome TEXT NOT NULL, Telefone TEXT NOT NULL, Celular TEXT NOT NULL, Email TEXT NOT NULL);";
 
     public DbHelper(Context context){
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
